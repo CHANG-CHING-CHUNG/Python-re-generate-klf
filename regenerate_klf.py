407e535d1be0b9969db42579df5adcdd94a12921
john <john.chang@nexus-techltd.com>
2021-06-03 16:43:29 +0800

    update: join array with blank space

--- regenerate_klf.py
+++ regenerate_klf.py
@@ -13,14 +13,24 @@ with open(f"{input_klf_backup_path}48430$$48533$$J0H989-CP-01.txt") as file_in:
     rough_bin_number_index = None
     for n in range(len(lines)):
       if "DefectRecordSpec" in lines[n]:
-        print(lines[n])
+        
         rough_bin_number_index = lines[n].index("ROUGHBINNUMBER")
-      if "TiffFileName" in lines[n]:
-        print(lines[n])
+      # if "TiffFileName" in lines[n]:
+
       if "DefectList" in lines[n]:
         lines[n+1][rough_bin_number_index]="106"
-        print(lines[n+1])
+        
+      lines[n] = " ".join(lines[n])
+      print(lines[n])
 
+file_write =  open("48430$$48533$$J0H989-CP-01.txt","a")
+
+for line in lines:
+  file_write.write(line)
+  file_write.write("\n")
+
+file_write.close()
+# print(lines)
 # print(lines[-8])
 # print(lines[-7])
 # print(lines[-6])
